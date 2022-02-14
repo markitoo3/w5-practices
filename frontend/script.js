6039b5c73584fe95052330e76d95c8aab0826ff8
markitoo3 <dietrich.marko@gmail.com>
2022-02-14 16:33:16 +0100

    first part of the session - js

---
+++ frontend/script.js
@@ -0,0 +1,72 @@
+/* console.log(a)
+console.log(c)
+ */
+
+// console.log("Hello")
+// console.log('Sima')
+// console.log(`back
+// tick`)
+
+// console.log(typeof 2)
+// console.log(typeof NaN)
+// console.log(0/0)
+// console.log(2.14)
+
+// console.log(true)
+// console.log(false)
+
+// console.log(undefined)
+// console.log(null)
+
+// console.log(typeof undefined)
+// console.log(typeof null)
+/* 
+console.log({
+    "title": "Start Coding",
+    "type": "Solo,",
+    "difficulty": 4.1,
+    "usefulness": 4.2,
+    "enjoyability": 3.7,
+    "materials": 3.4,
+    "tasks": [
+        {
+            "title": "bank accounts",
+            "description": ""
+        },
+        {
+            "title": "100 doors",
+            "description": ""
+        },
+        {
+            "title": "word filter",
+            "description": ""
+        }
+    ]
+})
+ */
+
+/* var a = "Hello1"
+let b = "Hello2"
+const c = "Hello3"
+
+console.log(a)
+console.log(b)
+console.log(c) */
+
+/* global scope
+function scope
+block scope
+ */
+
+function d(){
+    if(1+1 === 2){
+    }
+    var a = "Hello1"
+    let b = "Hello2"
+    const c = "Hello3"
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+d()
+
